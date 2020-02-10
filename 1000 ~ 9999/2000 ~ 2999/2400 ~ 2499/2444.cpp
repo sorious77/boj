7d9a3fb14222f9a7fa0ba051a04f5a4738078a53
Lee Jeong-woo <sorious77@naver.com>
2020-02-11 00:50:10 +0900

    add file

---
+++ 1000 ~ 9999/2000 ~ 2999/2400 ~ 2499/2444.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int n;
+	cin >> n;
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n - i - 1; j++)
+			printf(" ");
+		for (int j = 0; j < i * 2 + 1; j++)
+			printf("*");
+		printf("\n");
+	}
+
+	for (int i = 0; i < n - 1; i++) {
+		for (int j = 0; j < i + 1; j++)
+			printf(" ");
+		for (int j = 0; j < 2 * (n - i - 1) - 1; j++)
+			printf("*");
+		printf("\n");
+	}
+}
