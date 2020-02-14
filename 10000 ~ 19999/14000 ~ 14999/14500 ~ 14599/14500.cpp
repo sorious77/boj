78f07c2d8ea64f0d4701ce1c7245d0eb61b5d754
Lee Jeong-woo <sorious77@naver.com>
2020-02-14 18:20:25 +0900

    20-02-14

---
+++ 10000 ~ 19999/14000 ~ 14999/14500 ~ 14599/14500.cpp
@@ -0,0 +1,98 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cstring>
+
+using namespace std;
+
+int N, M;
+
+void dfs(int x, int y, int count, int sum);
+void shape(int x, int y);
+void initVisit();
+
+int dir[4][2] = { {0,-1}, {0,1}, {-1,0}, {1,0} };
+
+bool visit[500][500];
+int board[500][500];
+int Max = -1;
+
+int main() {
+	cin >> N >> M;
+
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < M; j++) {
+			cin >> board[i][j];
+		}
+	}
+
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < M; j++) {
+			memset(visit, false, sizeof(visit));
+			visit[i][j] = true; // visit x,y -> true
+
+			dfs(j, i, 0, board[i][j]); // j : x, i : y
+
+			shape(j, i);
+		}
+	}
+
+	cout << Max;
+}
+
+void dfs(int x, int y, int count, int sum) {
+	if (count >= 3) {
+		if (Max < sum) {
+			Max = sum;
+		}
+		return;
+	}
+
+	for (int i = 0; i < 4; i++) {
+		int nextX = x + dir[i][0];
+		int nextY = y + dir[i][1];
+
+		if (0 <= nextX && nextX < M && 0 <= nextY && nextY < N) {
+			if (visit[nextY][nextX] == false) {
+				visit[nextY][nextX] = true;
+				dfs(nextX, nextY, count + 1, sum + board[nextY][nextX]);
+				visit[nextY][nextX] = false;
+			}
+		}
+	}
+}
+
+void shape(int x, int y) {
+	int sum;
+
+	if (y - 1 >= 0 && y + 1 < N && x + 1 < M) {
+		sum = board[y][x] + board[y - 1][x] + board[y + 1][x] + board[y][x + 1];
+		if (sum > Max)
+			Max = sum;
+	}
+
+	if (y - 1 >= 0 && x - 1 >= 0 && y + 1 < N) {
+		sum = board[y][x] + board[y - 1][x] + board[y + 1][x] + board[y][x - 1];
+		if (sum > Max)
+			Max = sum;
+	}
+
+	if (y - 1 >= 0 && x - 1 >= 0 && x + 1 < M) {
+		sum = board[y][x] + board[y - 1][x] + board[y][x - 1] + board[y][x + 1];
+		if (sum > Max)
+			Max = sum;
+	}
+
+	if (y + 1 < N && x - 1 >= 0 && x + 1 < M) {
+		sum = board[y][x] + board[y][x - 1] + board[y][x + 1] + board[y + 1][x];
+		if (sum > Max)
+			Max = sum;
+	}
+}
+
+void initVisit() {
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < M; j++)
+			visit[i][j] = false;
+	}
+}
---
+++ 10000 ~ 19999/14000 ~ 14999/14800 ~ 14899/14890.cpp
@@ -0,0 +1,189 @@
+#include <iostream>
+#include <cmath>
+#include <cstring>
+
+using namespace std;
+
+int N, L;
+
+int map[100][100];
+int result = 0;
+bool visit[100][100];
+
+void findRoad();
+
+int main() {
+	cin >> N >> L;
+
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			cin >> map[i][j];
+		}
+	}
+
+	findRoad();
+
+	//printf("Result : %d\n", result);
+	cout << result;
+}
+
+void findRoad() {
+	int prev;
+	int count = 0;
+	bool check = true;
+
+	for (int i = 0; i < N; i++) { // 가로 길
+		prev = map[i][0]; // 해당 길의 제일 왼쪽 좌표
+		count = 0;
+		check = true;
+
+		memset(visit, false, sizeof(visit));
+
+		for (int j = 0; j < N; j++) {
+			if (!check)
+				break;
+
+			if (prev == map[i][j]) // 같은 높이인 경우
+				count++; // 길이를 구함
+			else {
+				if (abs(prev - map[i][j]) >= 2) { // 높이 차이가 2 이상
+					check = false;
+					break;
+				}
+
+				if (prev > map[i][j]) { // 높이가 낮아진 경우
+					int start = j;
+					int temp = map[i][j];
+					count = 1;
+
+					while (j + 1 < N) { // 낮은 높이의 길이를 구함
+						if ((map[i][j + 1] != temp) || count >= L)
+							break;
+
+						j++;
+						count++;
+					}
+					if (count < L) { // 길이가 L보다 작으면 경사로를 놓을 수 없음
+						check = false;
+						break;
+					}
+
+					// 이미 경사로가 놓인 경우
+					for (int k = start; k <= j; k++) {
+						if (visit[i][k]) {
+							check = false;
+						}
+					}
+
+					// 경사로를 놓음
+					for (int k = start; k <= j; k++) {
+						visit[i][k] = true;
+					}
+					count = 0;
+				}
+				else { // 높이가 높아진 경우
+					if (count < L) { // 길이가 L보다 작으면 경사로를 놓을 수 없음
+						check = false;
+						break;
+					}
+
+					// 이미 경사로가 놓인 경우
+					for (int k = 0; k < L; k++) {
+						if (visit[i][j - k - 1]) {
+							check = false;
+						}
+					}
+
+					// 경사로를 놓음
+					for (int k = 0; k < L; k++) {
+						visit[i][j - k - 1] = true;
+					}
+					count = 1;
+				}
+				prev = map[i][j];
+			}
+		}
+
+		if (check) {
+			result++;
+		}
+	}
+	
+
+	for (int i = 0; i < N; i++) { // 세로 길
+		prev = map[0][i]; // 해당 길의 제일 위쪽 좌표
+		count = 0;
+		check = true;
+
+		memset(visit, false, sizeof(visit));
+
+		for (int j = 0; j < N; j++) {
+			if (!check)
+				break;
+
+			if (prev == map[j][i]) // 같은 높이인 경우
+				count++; // 길이를 구함
+			else {
+				if (abs(prev - map[j][i]) >= 2) { // 높이 차이가 2 이상
+					check = false;
+					break;
+				}
+
+				if (prev > map[j][i]) { // 높이가 낮아진 경우
+					int start = j;
+					int temp = map[j][i];
+					count = 1;
+
+					while (j + 1 < N) { // 낮은 높이의 길이를 구함
+						if ((map[j + 1][i] != temp) || count >= L)
+							break;
+
+						j++;
+						count++;
+					}
+					if (count < L) { // 길이가 L보다 작으면 경사로를 놓을 수 없음
+						check = false;
+						break;
+					}
+
+					// 이미 경사로가 놓인 경우
+					for (int k = start; k <= j; k++) {
+						if (visit[k][i]) {
+							check = false;
+						}
+					}
+
+					// 경사로를 놓음
+					for (int k = start; k <= j; k++) {
+						visit[k][i] = true;
+					}
+					count = 0;
+				}
+				else { // 높이가 높아진 경우
+					if (count < L) { // 길이가 L보다 작으면 경사로를 놓을 수 없음
+						check = false;
+						break;
+					}
+
+					// 이미 경사로가 놓인 경우
+					for (int k = 0; k < L; k++) {
+						if (visit[j - k - 1][i]) {
+							check = false;
+						}
+					}
+
+					// 경사로를 놓음
+					for (int k = 0; k < L; k++) {
+						visit[j - k - 1][i] = true;
+					}
+					count = 1;
+				}
+				prev = map[j][i];
+			}
+		}
+
+		if (check) {
+			result++;
+		}
+	}
+}
